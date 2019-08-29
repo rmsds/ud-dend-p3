4ee50f6df217984f015088e6f440564440dcb76d
Ricardo Silva <ricardo.d.silva@gmail.com>
2019-08-29 18:41:51 +0200

    create_tables.py: more verbosity and flake8 checks

--- create_tables.py
+++ create_tables.py
@@ -4,22 +4,36 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    print("DROPing tables: {} operation(s) to execute".format(
+        len(drop_table_queries)
+    ))
     for query in drop_table_queries:
+        print('DROPing table...', end='')
         cur.execute(query)
         conn.commit()
+        print('OK!')
 
 
 def create_tables(cur, conn):
+    print("CREATing tables: {} operation(s) to execute".format(
+        len(create_table_queries)
+    ))
     for query in create_table_queries:
+        print('CREATing table...', end='')
         cur.execute(query)
         conn.commit()
+        print('OK!')
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+            "host={} dbname={} user={} password={} port={}".format(
+                *config['CLUSTER'].values()
+            )
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
