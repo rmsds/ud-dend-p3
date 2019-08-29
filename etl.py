c853fb2aef8fc16eee0f3e1dbab814e98773c6e1
Ricardo Silva <ricardo.d.silva@gmail.com>
2019-08-29 18:36:49 +0200

    etl.py: more verbosity and flake8 checks

--- etl.py
+++ etl.py
@@ -4,24 +4,38 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    print("Loading staging tables: {} operation(s) to execute".format(
+        len(copy_table_queries)
+    ))
     for query in copy_table_queries:
+        print('Loading staging table...', end='')
         cur.execute(query)
         conn.commit()
+        print('OK!')
 
 
 def insert_tables(cur, conn):
+    print("Inserting into tables: {} operation(s) to execute".format(
+        len(insert_table_queries)
+    ))
     for query in insert_table_queries:
+        print('Inserting into table...', end='')
         cur.execute(query)
         conn.commit()
+        print('OK!')
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+            "host={} dbname={} user={} password={} port={}".format(
+                *config['CLUSTER'].values()
+            )
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
